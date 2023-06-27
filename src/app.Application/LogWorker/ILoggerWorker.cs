072d11e4c1628588908c658fc70e9c6be0842b8c
Rodrigo Abreu <rodrigo.s.abreu@gmail.com>
2023-06-27 12:21:06 -0300

    feat: ajuste nome da pasta de log

---
+++ src/app.Application/LogWorker/ILoggerWorker.cs
@@ -0,0 +1,10 @@
+﻿namespace app.Application.Log
+{
+    public interface ILoggerWorker<out T>
+    {
+        void LogException(string errorMessage, Exception e);
+        void LogWarning(string errorMessage);
+        void LogInfo(string errorMessage);
+        void LogInfo(Dictionary<string, object> logItens, string infoMessage);
+    }
+}
---
+++ src/app.Application/LogWorker/LoggerWorker.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace app.Application.Log
+{
+    public class LoggerWorker<T> : ILoggerWorker<T>
+    {
+        private readonly ILogger<T> _logger;
+
+        public LoggerWorker(ILogger<T> logger)
+        {
+            _logger = logger;
+        }
+
+        private static string GetClassName()
+        {
+            return typeof(T).Name;
+        }
+
+        public void LogException(string errorMessage, Exception e)
+        {
+            using (_logger.BeginScope(new Dictionary<string, object>
+            {
+                ["type"] = e.GetType().ToString(),
+                ["message"] = e.Message,
+                ["innerexception"] = e.InnerException,
+                ["stacktrace"] = e.StackTrace
+
+            }))_logger.LogError($"[{GetClassName()}] {errorMessage}");
+        }
+
+        public void LogInfo(string errorMessage)
+        {
+            _logger.LogInformation($"[{GetClassName()}] {errorMessage}");
+        }
+
+        public void LogInfo(Dictionary<string, object> logItens, string infoMessage)
+        {
+            using (_logger.BeginScope(logItens))_logger.LogInformation($"[{GetClassName()}] {infoMessage}");
+        }
+
+        public void LogWarning(string errorMessage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
